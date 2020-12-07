3a09bc617aaf9999f3e3f4a76ab62567a0bd4166
tomo <tomo12078831@gmail.com>
2020-12-07 13:44:15 +0000

    fix migrate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,15 +11,15 @@
        
        
 
-        <div class="container">
+        
              {{-- ナビゲーションバー --}}
         @include('commons.navbar')
 
-        <div class="container">
+        
             {{-- エラーメッセージ --}}
             @include('commons.error_tasks')
             @yield('content')
-        </div>
+        
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
